64c1283d9ce39923958b46780fa21c449146110f
Jonathan Barnes <jonathan@codeword.net>
2018-04-08 09:37:58 -0700

    allows paging from api/posts

--- server/src/main/java/com/pivotalhacker/demo/blog/PostRepository.java
+++ server/src/main/java/com/pivotalhacker/demo/blog/PostRepository.java
@@ -1,6 +1,6 @@
 package com.pivotalhacker.demo.blog;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostRepository extends CrudRepository<Post, Long>{
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 }
